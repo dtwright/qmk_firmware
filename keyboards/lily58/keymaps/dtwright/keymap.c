fb1bef8e8623961c174786bf0d080077025b4deb
Dan Wright <dan.wright@everactive.com>
2021-04-12 11:49:42 -0400

    Update keymap for tapdance mods

--- keyboards/lily58/keymaps/dtwright/keymap.c
+++ keyboards/lily58/keymaps/dtwright/keymap.c
@@ -11,6 +11,18 @@ enum layer_number {
   _ARROWS
 };
 
+enum {
+    TD_CMD_ALT = 0,
+    TD_ALT_CMD,
+    TD_CMD_CTL
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_CMD_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_RGUI, KC_RALT),
+    [TD_ALT_CMD] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_LGUI),
+    [TD_CMD_CTL] = ACTION_TAP_DANCE_DOUBLE(KC_LCMD, KC_LCTL),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
@@ -33,8 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                    KC_LALT, KC_LGUI, MO(_LOWER), LT(_ARROWS, KC_SPC), LT(_MOUSE, KC_SPC), MO(_RAISE), KC_RGUI, KC_RCTL \
+                    KC_LALT, TD(TD_CMD_CTL), MO(_LOWER), LT(_ARROWS, KC_SPC), LT(_MOUSE, KC_SPC), MO(_RAISE), TD(TD_CMD_ALT), KC_RCTL \
 ),
+
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |

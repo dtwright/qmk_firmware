c3d2b87a7af2044d41bd362ab9392774227d31c3
Dan Wright <dan.wright@everactive.com>
2021-05-05 15:16:12 -0400

    Put enter and backspace on left thumb layers

--- keyboards/lily58/keymaps/dtwright/keymap.c
+++ keyboards/lily58/keymaps/dtwright/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
   KC_GRV,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                    KC_LALT, TD(TD_CMD_CTL), MO(_LOWER), LT(_ARROWS, KC_SPC), LT(_MOUSE, KC_SPC), MO(_RAISE), TD(TD_CMD_ALT), KC_RCTL \
+                    KC_LALT, TD(TD_CMD_CTL), MO(_LOWER), LT(_ARROWS, KC_ENT), LT(_MOUSE, KC_SPC), MO(_RAISE), TD(TD_CMD_ALT), KC_RCTL \
 ),
 
 /* LOWER
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, KC_WH_U, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
   _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
   _______, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,\
-                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+                             _______, _______, _______, KC_BSPC, _______,  _______, _______, _______ \
   ),
 
   /* Arrows

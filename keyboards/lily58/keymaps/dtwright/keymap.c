638479d49b354440aeebb330eec62e97c208b48e
Dan Wright <dan.wright@everactive.com>
2021-05-18 11:57:44 -0400

    add some mouse button stuff for using with the Ploopy Nano

--- keyboards/lily58/keymaps/dtwright/keymap.c
+++ keyboards/lily58/keymaps/dtwright/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______,_______, _______, _______,\
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_PIPE, \
+  XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN4, KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_PIPE, \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_CIRC, KC_UNDS, KC_PLUS, KC_LPRN, KC_RPRN, _______, \
   KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, XXXXXXX, _______, _______, KC_HOME, KC_END,  _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_ARROWS] = LAYOUT( \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, XXXXXXX, _______, \
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______, \
+  _______, KC_BTN1, KC_BTN3, KC_BTN2, KC_BTN4, XXXXXXX,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX, _______, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, _______,\
                              _______, _______, _______, _______, KC_SPC,  _______, _______, _______ \
